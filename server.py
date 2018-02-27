66ea3647289b1ec376da62bc474018bd1f2756f5
jbjw <github@jbjw.com>
2018-02-27 15:58:52 -0800

    auto-save

--- server.py
+++ server.py
@@ -60,4 +60,6 @@ def api_dept():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80)
+#     from app import app
+# app.run(host='0.0.0.0', port=50000, debug=True)
